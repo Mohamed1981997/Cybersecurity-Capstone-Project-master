d0805a6b313667dc878aaaa0b057b3184b0c3a40
Mohamed Abd Elrahman <32969769+Mohamed1981997@users.noreply.github.com>
2018-03-14 20:28:46 +0200

    Update Master.php

--- Coursera/Connections/Master.php
+++ Coursera/Connections/Master.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_Master = "50.62.209.157";
+$hostname_Master = "localhost";
 $database_Master = "master";
-$username_Master = "test";
-$password_Master = "123456";
+$username_Master = "root";
+$password_Master = "";
 $Master = mysql_pconnect($hostname_Master, $username_Master, $password_Master) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
